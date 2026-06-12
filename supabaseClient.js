964c6e58c008a288e0a3b7fd0fef47b23613adc3
usertvde <usertvde@hotmail.com>
2026-06-12 19:26:04 +0000

    .

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,3 @@
-// Substitui com os valores do teu projeto!
 const SUPABASE_URL = "https://oaztprykumrbubqxjkmb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9henRwcnlrdW1yYnVicXhqa21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzk2NzYsImV4cCI6MjA5Njg1NTY3Nn0.dywR3Xt1Dv4w5nR1tjKkNnW3_v9muGmgT8Tr4ug6jCk";
 
