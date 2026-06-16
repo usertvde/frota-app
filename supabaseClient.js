1c4d07e57911cddd4f1a1cfe84b4c6b9eaa9d7ac
usertvde <usertvde@hotmail.com>
2026-06-16 15:07:22 +0000

    16062026

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
-var SUPABASE_URL = "https://oaztprykumrbubqxjkmb.supabase.co";
-var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9henRwcnlrdW1yYnVicXhqa21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzk2NzYsImV4cCI6MjA5Njg1NTY3Nn0.dywR3Xt1Dv4w5nR1tjKkNnW3_v9muGmgT8Tr4ug6jCk";
-var EDGE_FUNCTION_URL = "https://oaztprykumrbubqxjkmb.supabase.co/functions/v1/clever-processor";
+const SUPABASE_URL = "https://oaztprykumrbubqxjkmb.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9henRwcnlrdW1yYnVicXhqa21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzk2NzYsImV4cCI6MjA5Njg1NTY3Nn0.dywR3Xt1Dv4w5nR1tjKkNnW3_v9muGmgT8Tr4ug6jCk";
+const EDGE_FUNCTION_URL = "https://oaztprykumrbubqxjkmb.supabase.co/functions/v1/clever-processor";
 
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.EDGE_FUNCTION_URL = EDGE_FUNCTION_URL;
